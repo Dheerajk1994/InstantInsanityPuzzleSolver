fb5b8a5a42cdbf0d13ee8f72e953a8035f279b25
Dheerajk1994 <dheerajk.har@gmail.com>
2019-11-25 20:14:34 -0800

    Worked on memory management and adjacent side assignment

--- main.c
+++ main.c
@@ -8,15 +8,17 @@
 #define MATH_SQRT_THREE 1.7320508075
 #define NUMBER_OF_CUBES 100
 
+typedef struct Side Side;
+
 struct Side{
 	_Bool taken;
 	unsigned color;
-	struct Side* adjacentSide;
+	Side* adjacentSide;
 };
 
-struct Cube{
-	struct Side* sides;
-};
+typedef struct{
+	Side* sides;
+}Cube;
 
 int puzzleOneColorCalculation(int sideIndex){
 	return 1 + ((int)floor(sideIndex * MATH_PI)%100);
@@ -34,40 +36,64 @@ int puzzleFourColorCalculation(int sideIndex){
 	return 1 + ((int)floor(sideIndex * MATH_SQRT_THREE)%100);
 }
 
-void generateCube(char puzzle[],int (*calculateFunction)(int)){
-	struct Cube* cubes = (struct Cube*)malloc(sizeof(struct Cube) * NUMBER_OF_CUBES);
+void generateCube(Cube** cubes, char puzzle[],int (*calculateFunction)(int)){
+	*cubes = (Cube*)malloc(sizeof(Cube) * NUMBER_OF_CUBES);
 	unsigned sideIndex = 1;
-	struct Cube* cubeIterator = cubes;
-	struct Cube* end = cubes + sizeof(struct Cube) * NUMBER_OF_CUBES;
+	Cube* cubeIterator = *cubes;
+	Cube* end = *cubes + sizeof(Cube) * NUMBER_OF_CUBES;
 
-	for(end; cubeIterator != end; cubeIterator += sizeof(struct Cube)){
-		cubeIterator->sides = (struct Side*)malloc(sizeof(struct Side)*3);
+	for(end; cubeIterator != end; cubeIterator += sizeof(Cube)){
+		cubeIterator->sides = (Side*)malloc(sizeof(Side)*3);
+
+		cubeIterator->sides[0].adjacentSide = &(cubeIterator->sides[1]);
+		cubeIterator->sides[1].adjacentSide = &(cubeIterator->sides[2]);
+		cubeIterator->sides[2].adjacentSide = &(cubeIterator->sides[0]);
+
 		cubeIterator->sides[0].color = (*calculateFunction)(sideIndex++);
 		cubeIterator->sides[1].color = (*calculateFunction)(sideIndex++);
 		cubeIterator->sides[2].color = (*calculateFunction)(sideIndex++);
 	}
 
-	FILE* outputFile = fopen("cubes.txt", "a");
-	fprintf(outputFile, "%s:\n", puzzle);
-	cubeIterator = cubes;
-	int counter = 1;
-	for(end; cubeIterator != end; cubeIterator += sizeof(struct Cube)){
-		//printf("%i ", cubeIterator->sides[0].color);
-		//printf("\t%i ", cubeIterator->sides[1].color);
-		//printf("\t%i\n", cubeIterator->sides[2].color);
-		fprintf(outputFile, "Cube %d: ", counter++);
-		fprintf(outputFile, "\t%i\t%i\t%i\n", cubeIterator->sides[0].color, cubeIterator->sides[1].color, cubeIterator->sides[2].color);
-	}
-	fprintf(outputFile, "\n");
-	fclose(outputFile);
-
-	free(cubes);
+	//FILE* outputFile = fopen("cubes.txt", "w");
+	//fprintf(outputFile, "%s:\n", puzzle);
+	//cubeIterator = cubes;
+	//int counter = 1;
+	//for(end; cubeIterator != end; cubeIterator += sizeof(Cube)){
+	//	//printf("%i ", cubeIterator->sides[0].color);
+	//	//printf("\t%i ", cubeIterator->sides[1].color);
+	//	//printf("\t%i\n", cubeIterator->sides[2].color);
+	//	fprintf(outputFile, "Cube %d: ", counter++);
+	//	fprintf(outputFile, "\t%i\t%i\t%i\n", cubeIterator->sides[0].color, cubeIterator->sides[1].color, cubeIterator->sides[2].color);
+	//}
+	//fprintf(outputFile, "\n");
+	//fclose(outputFile);
 }	
 
+void freeUpMemory(Cube** cubes){
+	Cube* end = *cubes + sizeof(Cube) * NUMBER_OF_CUBES;
+	Cube* cubeIterator = *cubes;
+
+	for(end; cubeIterator != end; cubeIterator += sizeof(Cube)){
+		free(cubeIterator->sides);
+	}
+	free(*cubes);
+}
+
 int main(){
-	generateCube("Puzzle 1", puzzleOneColorCalculation);
-	generateCube("Puzzle 2", puzzleTwoColorCalculation);
-	generateCube("Puzzle 3", puzzleThreeColorCalculation);
-	generateCube("Puzzle 4", puzzleFourColorCalculation);
+	Cube* puzzleSet1;
+	Cube* puzzleSet2;
+	Cube* puzzleSet3;
+	Cube* puzzleSet4;
+
+	generateCube(&puzzleSet1,"Puzzle 1", puzzleOneColorCalculation);
+	generateCube(&puzzleSet2, "Puzzle 2", puzzleTwoColorCalculation);
+	generateCube(&puzzleSet3, "Puzzle 3", puzzleThreeColorCalculation);
+	generateCube(&puzzleSet4, "Puzzle 4", puzzleFourColorCalculation);
+
+	freeUpMemory(&puzzleSet1);
+	freeUpMemory(&puzzleSet2);
+	freeUpMemory(&puzzleSet3);
+	freeUpMemory(&puzzleSet4);
 	return 0;
 }
+
